5ec418df0ec7dd809354c6d5911578c569a51b01
lucaskahl <lucaschangkahl@gmail.com>
2019-12-27 16:09:17 -0300

    added PostItem component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+      slug="/about"
+      background="brown"
+      category="Misc"
+      date="27 de Dezembro de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Alguams razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
   </Layout>
 )
 
